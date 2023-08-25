81b7a141f9146b48fd7f5c832fae42d832bbea8e
cam <cambo9p.11@gmail.com>
2023-08-26 01:30:55 +1200

    correctly format js

--- static/js/updateTable.js
+++ static/js/updateTable.js
@@ -1,21 +1,22 @@
-document.getElementById("fetchButton").addEventListener("click", async () => {
-    try {
-        const response = await fetch("api/db/get_users");
-        const responseData = await response.json();
+/* this file takes the json from the api one a button is hit and displays it as a table */
+document.getElementById('fetchButton').addEventListener('click', async () => {
+  try {
+    const response = await fetch('api/db/get_users')
+    const responseData = await response.json()
 
-        const tableBody = document.getElementById("table-body");
-        tableBody.innerHTML = ""; // Clear existing content
+    const tableBody = document.getElementById('table-body')
+    tableBody.innerHTML = ''
 
-        responseData.rows.forEach(rowData => {
-            const tr = document.createElement("tr");
-            rowData.forEach(cellData => {
-                const td = document.createElement("td");
-                td.textContent = cellData;
-                tr.appendChild(td);
-            });
-            tableBody.appendChild(tr);
-        });
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-});
+    responseData.rows.forEach(rowData => {
+      const tr = document.createElement('tr')
+      rowData.forEach(cellData => {
+        const td = document.createElement('td')
+        td.textContent = cellData
+        tr.appendChild(td)
+      })
+      tableBody.appendChild(tr)
+    })
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+})
